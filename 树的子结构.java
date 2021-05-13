a8c38568f4afcd89921e72c6f2fac83b2647a182
deepbreath373 <1149735109@qq.com>
2021-05-13 21:59:02 +0800

    二叉树

---
+++ 树的子结构.java
@@ -0,0 +1,67 @@
+/**
+ * 输入两棵二叉树A和B，判断B是不是A的子结构。(约定空树不是任意一个树的子结构)
+ *
+ * B是A的子结构， 即 A中有出现和B相同的结构和节点值。
+ *
+ * 例如:
+ * 给定的树 A:
+ *
+ *      3
+ *     / \
+ *    4   5
+ *   / \
+ *  1   2
+ * 给定的树 B：
+ *
+ *    4
+ *   /
+ *  1
+ * 返回 true，因为 B 与 A 的一个子树拥有相同的结构和节点值。
+ *
+ * 示例 1：
+ *
+ * 输入：A = [1,2,3], B = [3,1]
+ * 输出：false
+ *
+ */
+public class 树的子结构 {
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public boolean isSubStructure(TreeNode A, TreeNode B) {
+        boolean result = false;
+        if (A != null && B != null) {
+            if (A.val == B.val) {
+                result = doseAHaveB(A, B);
+            }
+            if (!result) {
+                result = isSubStructure(A.left, B);
+            }
+            if (!result) {
+                result = isSubStructure(A.right, B);
+            }
+        }
+        return result;
+    }
+
+    private boolean doseAHaveB(TreeNode a, TreeNode b) {
+        //如果子树能够走到空，则前面的相等判断都通过
+        if(b == null){
+            return true;
+        }
+        if(a == null){
+            return false;
+        }
+        if(a.val != b.val){
+            return false;
+        }
+        return doseAHaveB(a.left,b.left) && doseAHaveB(a.right,b.right);
+    }
+}
