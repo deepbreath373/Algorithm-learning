0b785f7946b09190f303fadb84648e35401dc736
deepbreath373 <1149735109@qq.com>
2021-05-15 21:59:28 +0800

    二叉树

---
+++ 对称的二叉树.java
@@ -0,0 +1,53 @@
+/**
+ * 请实现一个函数，用来判断一棵二叉树是不是对称的。如果一棵二叉树和它的镜像一样，那么它是对称的。
+ *
+ * 例如，二叉树 [1,2,2,3,4,4,3] 是对称的。
+ *
+ *     1
+ *    / \
+ *   2   2
+ *  / \ / \
+ * 3  4 4  3
+ * 但是下面这个 [1,2,2,null,3,null,3] 则不是镜像对称的:
+ *
+ *     1
+ *    / \
+ *   2   2
+ *    \   \
+ *    3    3
+ *
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：root = [1,2,2,3,4,4,3]
+ * 输出：true
+ *
+ */
+public class 对称的二叉树 {
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public boolean isSymmetric(TreeNode root){
+        return isSymmetric(root,root);
+    }
+    public boolean isSymmetric(TreeNode root1,TreeNode root2){
+        if(root1 == null && root2 == null){
+            return true;
+        }
+        if(root1 == null || root2 == null){
+            return false;
+        }
+        if(root1.val != root2.val){
+            return false;
+        }
+        return isSymmetric(root1.left,root2.right) && isSymmetric(root1.right,root2.left);
+    }
+}
