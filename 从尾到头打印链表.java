ea05b445acda09f49fdddb37e59d1120bdd2be96
deepbreath373 <1149735109@qq.com>
2021-05-21 15:48:19 +0800

    增加内部类

--- 从尾到头打印链表.java
+++ 从尾到头打印链表.java
@@ -1,27 +1,27 @@
 import java.util.ArrayList;
 import java.util.Stack;
 
-/**
- * 输入一个链表的头节点，从尾到头反过来返回每个节点的值（用数组返回）。
- *
- * 示例 1：
- * 输入：head = [1,3,2]
- * 输出：[2,3,1]
- *
- * 限制：
- * 0 <= 链表长度 <= 10000
- *
- */
-class ListNode {
-    int val;
-    ListNode next = null;
-
-    ListNode(int val) {
-        this.val = val;
-    }
-}
-
 public class 从尾到头打印链表 {
+
+    /**
+     * 输入一个链表的头节点，从尾到头反过来返回每个节点的值（用数组返回）。
+     *
+     * 示例 1：
+     * 输入：head = [1,3,2]
+     * 输出：[2,3,1]
+     *
+     * 限制：
+     * 0 <= 链表长度 <= 10000
+     *
+     */
+    class ListNode {
+        int val;
+        ListNode next = null;
+
+        ListNode(int val) {
+            this.val = val;
+        }
+    }
     public static int[] reversePrint(ListNode head) {
         Stack<ListNode> stack = new Stack<>();
         while (head != null) {
--- 用两个栈实现队列.java
+++ 用两个栈实现队列.java
@@ -1,27 +1,27 @@
 import java.util.Stack;
 
-class CQueue{
-    Stack<Integer> stack1;
-    Stack<Integer> stack2;
-    public CQueue(){
-        stack1 = new Stack<>();
-        stack2 = new Stack<>();
-    }
-    public void appendTail(int value){
-        stack1.push(value);
-    }
-    public int deleteHead(){
-        if(stack2.isEmpty()){
-            while(!stack1.isEmpty()){
-                stack2.push(stack1.pop());
+public class 用两个栈实现队列 {
+    class CQueue{
+        Stack<Integer> stack1;
+        Stack<Integer> stack2;
+        public CQueue(){
+            stack1 = new Stack<>();
+            stack2 = new Stack<>();
+        }
+        public void appendTail(int value){
+            stack1.push(value);
+        }
+        public int deleteHead(){
+            if(stack2.isEmpty()){
+                while(!stack1.isEmpty()){
+                    stack2.push(stack1.pop());
+                }
+                return stack2.isEmpty()?-1:stack2.pop();
+            }else{
+                return stack2.pop();
             }
-            return stack2.isEmpty()?-1:stack2.pop();
-        }else{
-            return stack2.pop();
         }
     }
-}
-public class 用两个栈实现队列 {
 
 
 }
