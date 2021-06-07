9e802fc76003ac2c55e583d7b6473693c5f50589
deepbreath373 <1149735109@qq.com>
2021-06-07 17:09:07 +0800

    二叉树

--- 最小的k个数.java
+++ 最小的k个数.java
@@ -1,9 +1,7 @@
-import java.util.Arrays;
-import java.util.PriorityQueue;
-import java.util.Queue;
+import java.util.*;
 
 public class 最小的k个数 {
-    public int[] getLeastNumbers(int[] arr, int k){
+    public int[] getLeastNumbers(int[] arr, int k) {
         Arrays.sort(arr);
         int[] res = new int[k];
         for (int i = 0; i < k; i++) {
@@ -12,26 +10,28 @@ public class 最小的k个数 {
         return res;
     }
 
-    public int[] getLeastNumbers2(int[] arr, int k){
-        if(k==0 || arr.length==0){
+    public int[] getLeastNumbers2(int[] arr, int k) {
+        if (k == 0 || arr.length == 0) {
             return new int[0];
         }
-        return quickSearch(arr,0,arr.length-1,k-1);
+        return quickSearch(arr, 0, arr.length - 1, k - 1);
     }
+
     private int[] quickSearch(int[] arr, int lo, int hi, int k) {
-        int j = partiton(arr,lo,hi);
-        if(j==k){
-            return Arrays.copyOf(arr,j+1);
+        int j = partiton(arr, lo, hi);
+        if (j == k) {
+            return Arrays.copyOf(arr, j + 1);
         }
-        return j > k ? quickSearch(arr,lo,j-1,k) : quickSearch(arr,j+1,hi,k);
+        return j > k ? quickSearch(arr, lo, j - 1, k) : quickSearch(arr, j + 1, hi, k);
     }
+
     private int partiton(int[] arr, int lo, int hi) {
         int v = arr[lo];
         int i = lo, j = hi + 1;
-        while(true){
-            while(++i <= hi && arr[i] < v);
-            while(--j >= lo && arr[j] > v);
-            if(i >= j){
+        while (true) {
+            while (++i <= hi && arr[i] < v) ;
+            while (--j >= lo && arr[j] > v) ;
+            if (i >= j) {
                 break;
             }
             int t = arr[j];
@@ -44,15 +44,15 @@ public class 最小的k个数 {
     }
 
     //利用大根堆
-    public int[] getLeastNumbers3(int[] arr, int k){
-        if(k==0 || arr.length==0){
+    public int[] getLeastNumbers3(int[] arr, int k) {
+        if (k == 0 || arr.length == 0) {
             return new int[0];
         }
-        Queue<Integer> pq = new PriorityQueue<>((v1,v2)->v2-v1);
+        Queue<Integer> pq = new PriorityQueue<>((v1, v2) -> v2 - v1);
         for (int num : arr) {
-            if(pq.size() < k){
+            if (pq.size() < k) {
                 pq.offer(num);
-            }else if(num < pq.peek()){
+            } else if (num < pq.peek()) {
                 pq.poll();
                 pq.offer(num);
             }
@@ -63,5 +63,50 @@ public class 最小的k个数 {
             res[idx++] = num;
         }
         return res;
+    }
+
+    //利用二叉树
+    public int[] getLeastNumbers4(int[] arr, int k) {
+        class Solution {
+            public int[] getLeastNumbers(int[] arr, int k) {
+                if (k == 0 || arr.length == 0) {
+                    return new int[0];
+                }
+                // TreeMap的key是数字, value是该数字的个数。
+                // cnt表示当前map总共存了多少个数字。
+                TreeMap<Integer, Integer> map = new TreeMap<>();
+                int cnt = 0;
+                for (int num : arr) {
+                    // 1. 遍历数组，若当前map中的数字个数小于k，则map中当前数字对应个数+1
+                    if (cnt < k) {
+                        map.put(num, map.getOrDefault(num, 0) + 1);
+                        cnt++;
+                        continue;
+                    }
+                    // 2. 否则，取出map中最大的Key（即最大的数字), 判断当前数字与map中最大数字的大小关系：
+                    //    若当前数字比map中最大的数字还大，就直接忽略；
+                    //    若当前数字比map中最大的数字小，则将当前数字加入map中，并将map中的最大数字的个数-1。
+                    Map.Entry<Integer, Integer> entry = map.lastEntry();
+                    if (entry.getKey() > num) {
+                        map.put(num, map.getOrDefault(num, 0) + 1);
+                        if (entry.getValue() == 1) {
+                            map.pollLastEntry();
+                        } else {
+                            map.put(entry.getKey(), entry.getValue() - 1);
+                        }
+                    }
+                }
+                // 最后返回map中的元素
+                int[] res = new int[k];
+                int idx = 0;
+                for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+                    int freq = entry.getValue();
+                    while (freq-- > 0) {
+                        res[idx++] = entry.getKey();
+                    }
+                }
+                return res;
+            }
+        }
     }
 }
