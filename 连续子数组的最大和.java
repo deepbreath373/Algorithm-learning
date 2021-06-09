45306583673cca12ebb1a504ba1d87288ef71a18
deepbreath373 <1149735109@qq.com>
2021-06-09 16:34:08 +0800

    dp

---
+++ 连续子数组的最大和.java
@@ -0,0 +1,25 @@
+/**
+ * 输入一个整型数组，数组中的一个或连续多个整数组成一个子数组。求所有子数组的和的最大值。
+ * 要求时间复杂度为O(n)。
+ * 示例1:
+ *
+ * 输入: nums = [-2,1,-3,4,-1,2,1,-5,4]
+ * 输出: 6
+ * 解释: 连续子数组 [4,-1,2,1] 的和最大，为 6。
+ */
+public class 连续子数组的最大和 {
+    public int maxSubArray(int[] nums) {
+        int sum = nums[0], max = sum;
+        for (int i = 1; i < nums.length; i++) {
+            if(sum < 0){
+                sum = nums[i];
+            }else{
+                sum += nums[i];
+            }
+            if(sum > max){
+                max = sum;
+            }
+        }
+        return max;
+    }
+}
